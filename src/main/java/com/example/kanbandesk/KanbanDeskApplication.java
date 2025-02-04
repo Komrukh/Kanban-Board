9eedb3d5fd16e8aba1f55f7e0e7117077bf83f05
Komrukh <komaxamid88@gmail.com>
2025-02-04 18:22:42 +0500

    first commit

---
+++ src/main/java/com/example/kanbandesk/KanbanDeskApplication.java
@@ -0,0 +1,13 @@
+package com.example.kanbandesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbanDeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kanbandesk/KanbanDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kanbandesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
